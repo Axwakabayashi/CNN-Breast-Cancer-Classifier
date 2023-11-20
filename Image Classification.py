9187be2417e27b3cc1ce5773af03fdbe73bf10b3
Axwakabayashi <alex.wakabayashi1@gmail.com>
2023-11-19 19:26:23 -0500

    Added image recognition

--- Image Classification.py
+++ Image Classification.py
@@ -1,28 +1,14 @@
-import matplotlib
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
 import tensorflow as tf
 import os
-import cv2
-import imghdr
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
-
-data_dir = 'Breast-Cancer-Images'
+data_dir = os.path.join('CNN-Breast-Cancer-Classifier', 'Breast-Cancer-Images')
 image_exts = ['jpeg', 'jpg', 'png', 'bmp']
+print("Absolute path to data_dir:", os.path.abspath(data_dir))
 
-
-
-for image_class in os.listdir(data_dir):
-    for image in os.listdir(os.path.join(data_dir, image_class)):
-        image_path = os.path.join(data_dir, image_class, image)
-        try:
-            img = cv2.imread(image_path)
-            tip = imghdr.what(image_path)
-            if tip not in image_exts:
-                print('Image not in ext list {}'.format(image_path))
-                os.remove(image_path)
-        except Exception as e:
-            print('Issue with image {}'.format(image_path))
-            # os.remove(image_path)
-
+data = tf.keras.utils.image_dataset_from_directory('data')
